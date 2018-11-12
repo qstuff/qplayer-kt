57288cb7e8cc2d268f305d8f2cd825a5b0b8d215
Claus Chierici <github@antamauna.net>
2018-11-12 23:11:29 +0100

    added resources

--- app/src/main/java/org/qstuff/qplayer/PlayerActivity.kt
+++ app/src/main/java/org/qstuff/qplayer/PlayerActivity.kt
@@ -2,18 +2,12 @@ package org.qstuff.qplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import org.qstuff.qplayer.ui.player.PlayerFragment
 
 class PlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.player_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, PlayerFragment.newInstance())
-                .commitNow()
-        }
-    }
 
+    }
 }
